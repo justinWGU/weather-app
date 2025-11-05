8fdd64c1f44c9bdb0d971ecf1948f3c75f70374a
justinWGU <Justin_ortiz123@yahoo.com>
2025-11-04 19:13:55 -0800

    Adds custom hook layer for data acquisition

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,19 +1,49 @@
 import { useEffect, useState } from "react";
+import type { WeatherData } from "../Types/WeatherTypes";
+import { weatherService } from "../services/weatherService";
 
-export function useWeather( city: string ) {
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState/*<WeatherData | null>*/(null);
-
-  // weatherService()
-  useEffect( () => {
-    try {
-      // const data = weatherService(city);
-    } 
-    catch(err) {
-      console.error(err)
-    }
-  }, []);
-
-  // return { data, loading, error }
+export interface UseWeatherData {
+  error: boolean;
+  loading: boolean;
+  data: WeatherData | null;
 }
+
+export function useWeather( city: string ): UseWeatherData {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<WeatherData | null>(null);
+
+  useEffect( () => {
+    async function getData() {
+      try {
+        const data = await weatherService(city);
+        setLoading(false);
+        setData(data);
+      }
+      catch (err) {
+        setError(true);
+        console.error('Error occurred trying to get data: ', err);
+      }    
+    }
+    getData();
+  }, [city]);
+
+  return { error, loading, data };
+}
+
+    // const id = setInterval( async () => {
+    //   console.log('Getting data...');
+    //   let attempts = 0;
+    //   console.log('Attemps: ', attempts);
+    //   try {
+    //     const data = await weatherService(city);
+    //     setLoading(false);
+    //     setData(data);
+    //   } 
+    //   catch(err) {
+    //     if (attempts++ > 3) setError(true); 
+    //     console.error(err)
+    //   }
+    //   return;
+    // }, 2000);
+    // return () => clearInterval(id);
