7ecded5dd954a43b73e838c938894a74ea601a31
justinWGU <Justin_ortiz123@yahoo.com>
2025-11-04 19:13:02 -0800

    Adds types directory for those that are shared throughout the app

--- src/Types/WeatherTypes.tsx
+++ src/Types/WeatherTypes.tsx
@@ -1,7 +1,41 @@
-// interface APIWeatherData {
-  
-// }
+export interface CitySearchBoxProps {
+  setCity: React.Dispatch<React.SetStateAction<string>>;
+}
 
-// interface CurrentWeatherProps {
+export interface CurrentWeatherProps {
+  temp: number;
+  condition: number;  
+  city: string;
+}
 
-// }
+export interface ForecastDayProps {
+  dayOfWeek: string;
+  condition: number;
+  maxTemp: number;
+  minTemp: number;
+}
+
+export interface HourlyForecastProps {
+  hour: number;
+  condition: number;
+  temp: number;
+}
+
+export interface HourlyForecastContainerProps {
+  hours: Array<number>;
+  conditions: Array<number>;
+  temps: Array<number>;
+}
+
+export interface WeeklyForecastContainerProps {
+  daysOfWeek: Array<string>;
+  conditions: Array<number>;
+  maxTemps: Array<number>;
+  minTemps: Array<number>;
+}
+
+export interface WeatherData {
+  currentWeatherData: CurrentWeatherProps;
+  hourlyForecastData: HourlyForecastContainerProps;
+  weeklyForecastData: WeeklyForecastContainerProps;
+}
