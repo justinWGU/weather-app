576956f2504936fdb027415063f4dc0d72516a2b
justinWGU <Justin_ortiz123@yahoo.com>
2025-11-12 22:33:50 -0800

    Adds array object of image links for dynamically display of images

---
+++ src/utils/images.ts
@@ -0,0 +1,9 @@
+import reactImg from '../assets/react.svg';
+import sunny from '../assets/sunny.png';
+import cloudy from '../assets/cloudy.png';
+import rain from '../assets/rain.png';
+import lightning from '../assets/lightning.png';
+import partlyCloudy from '../assets/partlyCloudy.png';
+
+
+export const imgArr: Array<string> = [reactImg, sunny, cloudy, rain, lightning, partlyCloudy];
